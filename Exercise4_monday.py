7b619f7300cae991e743dee3c8cd599a8d59cba3
nynkeoosterhof <32043018+nynkeoosterhof@users.noreply.github.com>
2017-09-18 15:25:18 +0200

    Add files via upload

---
+++ Exercise4_monday.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 18 15:23:11 2017
+
+@author: nynkeoosterhof
+"""
+
+x = range(100)
+y = x
+combined = zip(x,y)
+print combined
