d4da09d3fd679984d34eed81af0ffae3d2449958
nynkeoosterhof <32043018+nynkeoosterhof@users.noreply.github.com>
2017-09-18 15:19:22 +0200

    Add files via upload

---
+++ Exercise3_monday.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 18 15:09:34 2017
+
+@author: nynkeoosterhof
+"""
+
+list = range(1,101)
+print list
+
+for i in list:
+    if ((i % 3) == 0) & ((i % 5) == 0):
+        print 'FizzBuzz'
+    elif i % 3 == 0:
+        print 'Fizz'
+    elif i % 5 == 0:
+        print 'Buzz'
+    else:
+        print i
